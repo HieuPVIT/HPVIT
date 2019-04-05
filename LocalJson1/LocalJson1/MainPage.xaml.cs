74ce40a4db75aec58ba673cfc660a48dd57029e6
HieuPVIT <hieu92pv@gmail.com>
2019-04-05 16:28:26 +0700

    XamarinJsonLc

---
+++ LocalJson1/LocalJson1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reflection;
+using LocalJson1.Models;
+using Newtonsoft.Json;
+
+
+
+namespace LocalJson1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<RootObject> _rootobj;
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("LocalJson1.herosima.json");
+
+
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+
+                List<RootObject> mylist = JsonConvert.DeserializeObject<List<RootObject>>(json);
+                _rootobj = new ObservableCollection<RootObject>(mylist);
+                MyListView.ItemsSource = _rootobj;
+            }
+        }
+    }
+}
---
+++ LocalJson1/LocalJson1/Models/RootObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalJson1.Models
+{
+    
+
+    public class RootObject
+    {
+        public int stationid { get; set; }
+        public string stationname { get; set; }
+        public string status { get; set; }
+       
+       
+    }
+}
